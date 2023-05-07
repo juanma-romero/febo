e26913a356811fb98a14063767d2cf826b20cace
Juanma Romero <xjuanmax@hotmail.com>
2023-05-07 12:34:56 -0400

    added adobe fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  ], 
   theme: {
     extend: {
+      fontFamily : {
+        'Pinot': ['pinot-grigio-modern', 'sans-serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
